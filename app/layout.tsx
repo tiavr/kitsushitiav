0c3ef5129ab37d98de7cfd0201a22e24c7ea6952
Tiav Randria <tiav.randria@gmail.com>
2025-07-30 21:22:24 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr-FR">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AnimatePresence mode="wait">{children}</AnimatePresence>
         <AudioPlayer />
